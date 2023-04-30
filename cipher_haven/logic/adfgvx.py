d9c0a6ed1123d5841c72d426c2b7585389ed00d1
Bhavesh Patil <dadu.patil@gmail.com>
2023-04-30 10:36:19 +0530

    Setup pytest; implement Rich Table

--- cipher_haven/logic/adfgvx.py
+++ cipher_haven/logic/adfgvx.py
@@ -1,4 +1,6 @@
 import numpy
+from rich.console import Console
+from rich.table import Table
 from string import ascii_lowercase
 
 
@@ -8,32 +10,24 @@ class ADFGVX:
         self.code_table = ["A", "D", "F", "G", "V", "X"]
 
     def __generate_code(self, code: str) -> list:
-        # TODO: This is a disgrace! Change it.
-        alphabets: list = list(map(str, ascii_lowercase))
+        alphabets: list = list(ascii_lowercase)
 
         extra_letters: list = []
         for letter in alphabets:
             if letter not in code:
                 extra_letters.append(letter)
 
-        code_list_with_duplicates: list = list(map(str, code))
-        code_list: list = []
+        code_list_with_duplicates: list = list(code)
+        code_list_without_duplicates: list = list(
+            {item: None for item in code_list_with_duplicates}
+        )
 
-        # TODO: Use Dict comprehenssion to order instead. Dicts are always ordered. It's neat! "{x: None for x in unordered_list}"
-        [
-            code_list.append(letter)
-            for letter in code_list_with_duplicates
-            if letter not in code_list
-        ]
-
-        sorted_letters: list = code_list + extra_letters
+        sorted_letters: list = code_list_without_duplicates + extra_letters
         table_key: list = []
 
         for letter in sorted_letters:
             table_key.append(letter)
-
             number: int = alphabets.index(letter) + 1
-
             if number <= 10:
                 table_key.append(number if number != 10 else 0)
 
@@ -42,13 +36,22 @@ class ADFGVX:
     def generate_table(self, code: str) -> None:
         table_key = numpy.array(self.__generate_code(code.lower()))
 
-        print(table_key)
+        # print(table_key)
         # ADFGVX table has 6 rows and columns
         self.table: numpy.ndarray = table_key.reshape(6, 6)
 
-        # TODO: Maybe have a more readable version of the table with the ADFGVX embeded?
-        print("Table Generated")
-        print(self.table)
+        table = Table(title="ADFGVX", show_lines=True)
+
+        table.add_column(" ")
+        for y in range(len(self.table)):
+            table.add_column(self.code_table[y])
+
+        for y in range(len(self.table)):
+            table_row = [self.code_table[y]] + list(self.table[y])
+            table.add_row(*table_row)
+
+        console = Console()
+        console.print(table)
 
     def encrypt(self, message: str, key: str):
         message_no_spaces = message.replace(" ", "").lower()
@@ -71,9 +74,9 @@ class ADFGVX:
             code_string[i : i + len(key)] for i in range(0, len(code_string), len(key))
         ]
 
-        print(enciphered_plaintext)
+        return code_string
 
 
 cipher = ADFGVX()
-cipher.generate_table("SOMETHINGFOREVERYONE")
-cipher.encrypt("Honey I shrunk the kids", "NOTTHEBEES")
+cipher.generate_table("nachtbommenwerper")
+# cipher.encrypt("Honey I shrunk the kids", "NOTTHEBEES")
--- tests/logic/test_adfgvx.py
+++ tests/logic/test_adfgvx.py
@@ -1,4 +1,20 @@
-def test_foo():
-    a = 4 + 4
-    b = a + 9
-    assert a == b
+import pytest
+from cipher_haven.logic import adfgvx
+
+
+@pytest.mark.parametrize(
+    "table_code, message, code_key, expected_output",
+    [
+        (
+            "nachtbommenwerper",
+            "attack at 1200am",
+            "privacy",
+            "ADDDDDADAGVGADDDAFDGVFVFADDX",
+        )
+    ],
+)
+def test_cipher_run(table_code: str, message: str, code_key: str, expected_output: str):
+    cipher = adfgvx.ADFGVX()
+    cipher.generate_table(table_code)
+    plaintext = cipher.encrypt(message, code_key)
+    assert plaintext == expected_output
