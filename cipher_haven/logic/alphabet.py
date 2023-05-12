3b51990740c734c5d2f9f8e3c1aa48af956a9b3a
Bhavesh Patil <dadu.patil@gmail.com>
2023-05-13 00:38:02 +0530

    Decrypt Done

--- cipher_haven/logic/alphabet.py
+++ cipher_haven/logic/alphabet.py
@@ -1,39 +1,50 @@
-import numpy
+""" Alphabet Cipher """
+
 from copy import deepcopy
+from string import ascii_lowercase, ascii_uppercase
 from rich.console import Console
 from rich.table import Table, box
-from string import ascii_lowercase, ascii_uppercase
+import numpy
 
 
 class ALPHABET:
+    """Alphabet Cipher Class"""
+
     def __init__(self) -> None:
-        self.table = None
+        self.table: numpy.ndarray = None
+        self.keyword_list: list = []
         self.__generate_table()
 
     def __generate_table(self) -> None:
-        # loop_index: int = 0
         ascii_table = list(ascii_lowercase)
+
         table_lists: list = []
 
         for _ in ascii_table:
             localtable: list = deepcopy(ascii_table)
             table_lists += localtable
+
             first_letter: str = ascii_table.pop(0)
             ascii_table.append(first_letter)
 
         table_array = numpy.array(table_lists)
+
         self.table = table_array.reshape(26, 26)
 
     def print_table(self) -> bool:
-        if self.table == None:
+        """Prints the full Alphabet table"""
+
+        if self.table is None:
             return False
 
         table_print = Table(title="Alphabet", show_lines=True, box=box.SQUARE)
+
         table_print.add_column(" ")
-        for i in range(len(self.table)):
+
+        for i, _ in enumerate(self.table):
             table_print.add_column(ascii_uppercase[i])
 
-        for i in range(len(self.table)):
+        for i, _ in enumerate(self.table):
             table_row = [ascii_uppercase[i]] + list(self.table[i])
             table_print.add_row(*table_row)
 
@@ -42,30 +53,43 @@ class ALPHABET:
 
         return True
 
-    def encrypt(self, message: str, keyword: str) -> str:
-        plaintext: str = message.upper().replace(" ", "")
-
-        keyword_list: list = []
+    def __prepare_keyword(self, message_string: str, keyword: str) -> None:
         keyword_index: int = 0
-        for _ in range(len(plaintext)):
-            keyword_list.append(keyword[keyword_index].upper())
+
+        for _ in range(len(message_string)):
+            self.keyword_list.append(keyword[keyword_index].upper())
             keyword_index = keyword_index + 1 if keyword_index < len(keyword) - 1 else 0
 
-        print(keyword_list)
+    def encrypt(self, message: str, keyword: str) -> str:
+        """Encrypt the Message using the Alphabet Cipher"""
+
+        plaintext: str = message.upper().replace(" ", "")
+        self.__prepare_keyword(plaintext, keyword)
 
         encrypted_message: str = ""
-        for i in range(len(plaintext)):
-            l: str = plaintext[i]
 
-            row: int = ascii_uppercase.index(keyword_list[i])
-            column: int = ascii_uppercase.index(l)
+        for i, message_letter in enumerate(plaintext):
+            row: int = ascii_uppercase.index(self.keyword_list[i])
+            column: int = ascii_uppercase.index(message_letter)
 
             letter: str = self.table[row, column]
-
             encrypted_message += letter
 
         return encrypted_message
 
+    def decrypt(self, encrypted_message: str, keyword: str) -> str:
+        """Decrypt the Encrypted Message using the Alphabet Cipher"""
 
-al = ALPHABET()
-print(al.encrypt("meet me on Tuesday evening at seven", "vigilance"))
+        encrypted_message = encrypted_message.upper().replace(" ", "")
+        self.__prepare_keyword(encrypted_message, keyword)
+
+        decrypted_message: str = ""
+        for i, letter in enumerate(encrypted_message):
+            keyletter: str = self.keyword_list[i]
+
+            row: int = ascii_uppercase.index(keyletter)
+            column: int = list(self.table[row]).index(letter.lower())
+
+            decrypted_message += ascii_uppercase[column]
+
+        return decrypted_message
--- tests/logic/test_alphabet.py
+++ tests/logic/test_alphabet.py
@@ -1,3 +1,5 @@
+""" Alphabet Cipher Test Module """
+
 import pytest
 from cipher_haven.logic import alphabet
 
@@ -13,6 +15,26 @@ from cipher_haven.logic import alphabet
     ],
 )
 def test_encrypt(message: str, keyword: str, expected_output: str):
+    """Test the Alphabet Cipher Class encrypt function"""
+
     cipher = alphabet.ALPHABET()
-    encryped_message = cipher.encrypt(message, keyword)
-    assert encryped_message == expected_output
+    encrypted_message = cipher.encrypt(message, keyword)
+    assert encrypted_message == expected_output
+
+
+@pytest.mark.parametrize(
+    "encrypted_message, keyword, expected_output",
+    [
+        (
+            "hmkbxebpxpmyllyrxiiqtoltfgzzv",
+            "vigilance",
+            "meetmeontuesdayeveningatseven",
+        )
+    ],
+)
+def test_decrypt(encrypted_message: str, keyword: str, expected_output: str):
+    """Test the Alphabet Cipher Class decrypt function"""
+
+    cipher = alphabet.ALPHABET()
+    decrypted_message = cipher.decrypt(encrypted_message, keyword)
+    assert decrypted_message == expected_output
