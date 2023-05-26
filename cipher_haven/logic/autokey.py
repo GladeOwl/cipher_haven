d35d095df92c7474489bf39b3b2b77ebb5d6ea6b
Bhavesh Patil <patilbhavesh3197@gmail.com>
2023-05-26 19:26:35 +0530

    Autokey (#7)
    * Encrypt
    
    * Decrypt
    
    * Readme Addition
    
    ---------
    
    Co-authored-by: Bhavesh Patil <dadu.patil@gmail.com>

---
+++ cipher_haven/logic/autokey.py
@@ -0,0 +1,81 @@
+""" Autokey Cipher """
+
+from copy import deepcopy
+from string import ascii_uppercase
+from rich.console import Console
+from rich.table import Table, box
+import numpy
+
+
+class AUTOKEY:
+    """Autokey Cipher Class"""
+
+    def __init__(self) -> None:
+        self.__generate_table()
+
+    def __generate_table(self) -> None:
+        ascii_table = list(ascii_uppercase)
+        table_lists: list = []
+
+        for _ in ascii_table:
+            localtable: list = deepcopy(ascii_table)
+            table_lists += localtable
+
+            first_letter: str = ascii_table.pop(0)
+            ascii_table.append(first_letter)
+
+        table_array = numpy.array(table_lists)
+        self.table = table_array.reshape(26, 26)
+
+    def print_table(self) -> bool:
+        """Prints the full Alphabet table"""
+
+        if self.table is None:
+            return False
+
+        table_print = Table(title="Autokey", show_lines=True, box=box.SQUARE)
+
+        table_print.add_column(" ")
+
+        for i, _ in enumerate(self.table):
+            table_print.add_column(ascii_uppercase[i])
+
+        for i, _ in enumerate(self.table):
+            table_row = [ascii_uppercase[i]] + list(self.table[i])
+            table_print.add_row(*table_row)
+
+        console = Console()
+        console.print(table_print)
+
+        return True
+
+    def encrypt(self, message: str, key: str) -> str:
+        """Encrypt the Message using the Autokey Cipher"""
+        plaintext: str = message.upper().replace(" ", "")
+        plainkey: str = key.upper() + plaintext
+
+        encrypted_message: str = ""
+        for i, letter in enumerate(plaintext):
+            keyletter: str = plainkey[i]
+
+            row: int = ascii_uppercase.index(letter)
+            column: int = ascii_uppercase.index(keyletter)
+
+            encrypted_letter: str = self.table[row, column]
+            encrypted_message += encrypted_letter
+        return encrypted_message
+
+    def decrypt(self, encrypted_message: str, key: str) -> str:
+        """Decrypt the Message using the Autokey Cipher"""
+        decrypted_message: str = ""
+        for i, letter in enumerate(encrypted_message):
+            keyletter: str = key[i].upper()
+
+            row: int = ascii_uppercase.index(keyletter)
+            column: int = list(self.table[row]).index(letter)
+
+            decrypted_letter = ascii_uppercase[column]
+            key += decrypted_letter
+            decrypted_message += decrypted_letter
+
+        return decrypted_message
---
+++ tests/logic/test_autokey.py
@@ -0,0 +1,32 @@
+""" Autokey Cipher Test Module """
+
+import pytest
+from cipher_haven.logic import autokey
+
+
+@pytest.mark.parametrize(
+    "message, key, expected_output",
+    [
+        ("attack at dawn", "QUEENLY", "QNXEPVYTWTWP"),
+        ("meetatthefountain", "kilt", "WMPMMXXAEYHBRYOCA"),
+    ],
+)
+def test_encrypt(message: str, key: str, expected_output: str):
+    """Test the Autokey Cipher Class encrypt function"""
+    cipher = autokey.AUTOKEY()
+    encrypted_message = cipher.encrypt(message, key)
+    assert encrypted_message == expected_output
+
+
+@pytest.mark.parametrize(
+    "encrypted_message, key, expected_output",
+    [
+        ("QNXEPVYTWTWP", "QUEENLY", "ATTACKATDAWN"),
+        ("WMPMMXXAEYHBRYOCA", "kilt", "MEETATTHEFOUNTAIN"),
+    ],
+)
+def test_decrypt(encrypted_message: str, key: str, expected_output: str):
+    """Test the Autokey Cipher Class encrypt function"""
+    cipher = autokey.AUTOKEY()
+    encrypted_message = cipher.decrypt(encrypted_message, key)
+    assert encrypted_message == expected_output
