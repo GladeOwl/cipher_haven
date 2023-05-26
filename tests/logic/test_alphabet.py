569416082297791ecbceea6495a2e5dc12572f55
Bhavesh Patil <patilbhavesh3197@gmail.com>
2023-05-26 15:44:42 +0530

    Alphabet (#4)
    * Generate Alphabet Table
    
    * Test Encrypt Done
    
    * Decrypt Done
    
    ---------
    
    Co-authored-by: Bhavesh Patil <dadu.patil@gmail.com>

---
+++ tests/logic/test_alphabet.py
@@ -0,0 +1,40 @@
+""" Alphabet Cipher Test Module """
+
+import pytest
+from cipher_haven.logic import alphabet
+
+
+@pytest.mark.parametrize(
+    "message, keyword, expected_output",
+    [
+        (
+            "meet me on Tuesday evening at seven",
+            "vigilance",
+            "hmkbxebpxpmyllyrxiiqtoltfgzzv",
+        )
+    ],
+)
+def test_encrypt(message: str, keyword: str, expected_output: str):
+    """Test the Alphabet Cipher Class encrypt function"""
+
+    cipher = alphabet.ALPHABET()
+    encrypted_message = cipher.encrypt(message, keyword)
+    assert encrypted_message == expected_output
+
+
+@pytest.mark.parametrize(
+    "encrypted_message, keyword, expected_output",
+    [
+        (
+            "hmkbxebpxpmyllyrxiiqtoltfgzzv",
+            "vigilance",
+            "meetmeontuesdayeveningatseven",
+        )
+    ],
+)
+def test_decrypt(encrypted_message: str, keyword: str, expected_output: str):
+    """Test the Alphabet Cipher Class decrypt function"""
+
+    cipher = alphabet.ALPHABET()
+    decrypted_message = cipher.decrypt(encrypted_message, keyword)
+    assert decrypted_message == expected_output
