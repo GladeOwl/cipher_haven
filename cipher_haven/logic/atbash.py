74815d4223a92c945fbf075669d84efc5dc7ce86
Bhavesh Patil <dadu.patil@gmail.com>
2023-06-02 19:10:27 +0530

    Refactor Atbash

--- cipher_haven/logic/atbash.py
+++ cipher_haven/logic/atbash.py
@@ -2,28 +2,28 @@
 
 from string import ascii_uppercase
 
+ALPHABETS: list = list(ascii_uppercase)
+ALPHABETS_REVERSED: list = list(reversed(ALPHABETS))
+
 
 class ATBASH:
     """Atbash Cipher Class"""
 
     def __init__(self) -> None:
-        self.alphabets: list = list(ascii_uppercase)
-        self.alphabets_reversed: list = list(reversed(self.alphabets))
+        """No setup is required for this one."""  # TODO. Can we remove this?
 
     def encrypt(self, message: str) -> str:
         """Encrypt the Message using the Atbash Cipher"""
+
+        plaintext: str = message.upper().replace(" ", "")
         return "".join(
-            [
-                self.alphabets_reversed[self.alphabets.index(letter)]
-                for letter in message.upper().replace(" ", "")
-            ]
+            [ALPHABETS_REVERSED[ALPHABETS.index(letter)] for letter in plaintext]
         )
 
     def decrypt(self, encrypted_message: str) -> str:
         """Decrypt the Encrypted Message using the Atbash Cipher"""
+
+        encrypted_text: str = encrypted_message.upper().replace(" ", "")
         return "".join(
-            [
-                self.alphabets[self.alphabets_reversed.index(letter)]
-                for letter in encrypted_message.upper().replace(" ", "")
-            ]
+            [ALPHABETS[ALPHABETS_REVERSED.index(letter)] for letter in encrypted_text]
         )
