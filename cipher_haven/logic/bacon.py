0d4c9590b145a4aa14bb836fe6148a1b7c3c8465
Bhavesh Patil <dadu.patil@gmail.com>
2023-06-05 17:00:43 +0530

    Decrypt

--- cipher_haven/logic/bacon.py
+++ cipher_haven/logic/bacon.py
@@ -34,6 +34,9 @@ class BACON:
     """Bacon's Cipher Class"""
 
     def __init__(self, A: str = "A", B: str = "B") -> None:
+        if A == B:
+            raise ValueError("A and B values cannot be the same")
+
         self.able = A
         self.baker = B
 
@@ -45,3 +48,27 @@ class BACON:
             .replace("A", self.able)
             .replace("B", self.baker)
         )
+
+    def decrypt(self, encrypted_message: str) -> str:
+        """Decrypt the Message using the Bacon's Cipher"""
+        encrypted_message: str = (
+            encrypted_message.upper()
+            .replace(" ", "")
+            .replace("A", self.able)
+            .replace("B", self.baker)
+        )
+
+        if len(encrypted_message) % 5 != 0:
+            raise ValueError("Please input a valid encrypted message")
+
+        message_list: list = [
+            encrypted_message[i : i + 5] for i in range(0, len(encrypted_message), 5)
+        ]
+
+        letters: list = list(CODE_TABLE.keys())
+        letter_codes: list = list(CODE_TABLE.values())
+
+        decrypted_message: list = [
+            letters[letter_codes.index(x.lower())] for x in message_list
+        ]
+        return "".join(decrypted_message)
