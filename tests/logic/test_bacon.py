497e97fcb7a31f0bc24c1bdea51a242d71f1ed5c
Bhavesh Patil <patilbhavesh3197@gmail.com>
2023-06-14 19:04:48 +0530

    Remove A, B arguments (#22)
    Co-authored-by: Bhavesh Patil <dadu.patil@gmail.com>

--- tests/logic/test_bacon.py
+++ tests/logic/test_bacon.py
@@ -5,36 +5,32 @@ from cipher_haven.logic import bacon
 
 
 @pytest.mark.parametrize(
-    "message, A, B, expected_ouput",
+    "message, expected_ouput",
     [
         (
             "Hello There",
-            "A",
-            "B",
             "AABBB AABAA ABABB ABABB ABBBA BAABB AABBB AABAA BAAAB AABAA",
         )
     ],
 )
-def test_encrypt(message: str, A: str, B: str, expected_ouput: str):
+def test_encrypt(message: str, expected_ouput: str):
     """Test the Bacon's Cipher Class encrypt function"""
-    cipher = bacon.BACON(A, B)
+    cipher = bacon.BACON()
     encrypted_message = cipher.encrypt(message)
     assert encrypted_message == expected_ouput
 
 
 @pytest.mark.parametrize(
-    "encrypted_message, A, B, expected_ouput",
+    "encrypted_message, expected_ouput",
     [
         (
             "AABBB AABAA ABABB ABABB ABBBA BAABB AABBB AABAA BAAAB AABAA",
-            "A",
-            "B",
             "HELLOTHERE",
         )
     ],
 )
-def test_decrypt(encrypted_message: str, A: str, B: str, expected_ouput: str):
+def test_decrypt(encrypted_message: str, expected_ouput: str):
     """Test the Bacon's Cipher Class encrypt function"""
-    cipher = bacon.BACON(A, B)
+    cipher = bacon.BACON()
     decrypted_message = cipher.decrypt(encrypted_message)
     assert decrypted_message == expected_ouput
