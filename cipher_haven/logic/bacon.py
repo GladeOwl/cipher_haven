4d42b25848881f78a4db23e5618b262c17c50a25
Bhavesh Patil <patilbhavesh3197@gmail.com>
2023-06-14 18:56:17 +0530

    Refactor Bacon; Remove A, B replacer (#21)
    Co-authored-by: Bhavesh Patil <dadu.patil@gmail.com>

--- cipher_haven/logic/bacon.py
+++ cipher_haven/logic/bacon.py
@@ -1,65 +1,52 @@
 """ Bacon's Cipher """
 
+from cipher_haven.logic.cipher import CIPHER
+
 CODE_TABLE: dict = {
-    "A": "aaaaa",
-    "B": "aaaab",
-    "C": "aaaba",
-    "D": "aaabb",
-    "E": "aabaa",
-    "F": "aabab",
-    "G": "aabba",
-    "H": "aabbb",
-    "I": "abaaa",
-    "J": "abaab",
-    "K": "ababa",
-    "L": "ababb",
-    "M": "abbaa",
-    "N": "abbab",
-    "O": "abbba",
-    "P": "abbbb",
-    "Q": "baaaa",
-    "R": "baaab",
-    "S": "baaba",
-    "T": "baabb",
-    "U": "babaa",
-    "V": "babab",
-    "W": "babba",
-    "X": "babbb",
-    "Y": "bbaaa",
-    "Z": "bbaab",
+    "A": "AAAAA",
+    "B": "AAAAB",
+    "C": "AAABA",
+    "D": "AAABB",
+    "E": "AABAA",
+    "F": "AABAB",
+    "G": "AABBA",
+    "H": "AABBB",
+    "I": "ABAAA",
+    "J": "ABAAB",
+    "K": "ABABA",
+    "L": "ABABB",
+    "M": "ABBAA",
+    "N": "ABBAB",
+    "O": "ABBBA",
+    "P": "ABBBB",
+    "Q": "BAAAA",
+    "R": "BAAAB",
+    "S": "BAABA",
+    "T": "BAABB",
+    "U": "BABAA",
+    "V": "BABAB",
+    "W": "BABBA",
+    "X": "BABBB",
+    "Y": "BBAAA",
+    "Z": "BBAAB",
 }
 
 
-class BACON:
+class BACON(CIPHER):
     """Bacon's Cipher Class"""
-
-    def __init__(self, A: str = "A", B: str = "B") -> None:
-        if A == B:
-            raise ValueError("A and B values cannot be the same")
-
-        self.able = A
-        self.baker = B
 
     def encrypt(self, message: str) -> str:
         """Encrypt the Message using the Bacon's Cipher"""
+
         plaintext: str = message.upper().replace(" ", "")
-        return (
-            " ".join([CODE_TABLE[letter].upper() for letter in plaintext])
-            .replace("A", self.able)
-            .replace("B", self.baker)
-        )
+        encrypted_message: str = " ".join([CODE_TABLE[letter] for letter in plaintext])
+
+        return encrypted_message
 
     def decrypt(self, encrypted_message: str) -> str:
         """Decrypt the Message using the Bacon's Cipher"""
-        encrypted_message: str = (
-            encrypted_message.upper()
-            .replace(" ", "")
-            .replace("A", self.able)
-            .replace("B", self.baker)
-        )
 
-        if len(encrypted_message) % 5 != 0:
-            raise ValueError("Please input a valid encrypted message")
+        encrypted_message: str = encrypted_message.upper().replace(" ", "")
 
         message_list: list = [
             encrypted_message[i : i + 5] for i in range(0, len(encrypted_message), 5)
@@ -69,6 +56,7 @@ class BACON:
         letter_codes: list = list(CODE_TABLE.values())
 
         decrypted_message: list = [
-            letters[letter_codes.index(x.lower())] for x in message_list
+            letters[letter_codes.index(letter)] for letter in message_list
         ]
+
         return "".join(decrypted_message)
